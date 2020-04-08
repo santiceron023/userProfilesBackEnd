a48c0627f0acd8640a893278c43779047df20e25
Santiago Felipe Cerón Araujo - Ceiba Software <santiago.ceron@ceiba.com.co>
2020-04-08 17:04:09 -0500

    List by id issue of listing always same regiser resolved

--- src/infraestructura/user/adaptador/dao/dao-usuario-mongo.ts
+++ src/infraestructura/user/adaptador/dao/dao-usuario-mongo.ts
@@ -4,6 +4,7 @@ import { DaoUsuario } from 'src/dominio/usuario/puerto/dao/dao-usuario';
 import { UsuarioDTO } from 'src/dominio/usuario/modelo/usuario.dto';
 import { UsuarioEntidad } from '../../entidad/usuario.entidad';
 import * as mongodb from 'mongodb';
+import { ErrorDeNegocio } from 'src/dominio/excepciones/error-de-negocio';
 
 export class DaoUsuarioMongo implements DaoUsuario {
   constructor(
@@ -12,10 +13,14 @@ export class DaoUsuarioMongo implements DaoUsuario {
   ) { }
 
   async getById(userId: string): Promise<UsuarioDTO> {
-    let documentID = new mongodb.ObjectId(userId);
-    return await this.entityManager.findOne(UsuarioEntidad, {});
+    try {
+      let documentID = new mongodb.ObjectId(userId);
+      return await this.entityManager.findOne(UsuarioEntidad, documentID);
+    } catch (error) {
+      throw new ErrorDeNegocio('incorrect ID search');
+    }
   }
- 
+
   async listar(): Promise<UsuarioDTO[]> {
     return await this.entityManager.find(UsuarioEntidad, {});
   }
